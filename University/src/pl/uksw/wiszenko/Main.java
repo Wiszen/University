ad587468bb89395edd7779d4305080c7a1f88779
Mikolaj <Mikolaj@Wiszenko>
2017-03-20 15:57:40 +0100

    Init project

---
+++ University/src/pl/uksw/wiszenko/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package pl.uksw.wiszenko;
+
+/**
+ * @author Mikolaj
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ University/src/pl/uksw/wiszenko/Person.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package pl.uksw.wiszenko;
+
+/**
+ * Klasa abstrakcyjna reprezentujaca osobe na uczelni.
+ * 
+ * @author Mikolaj
+ *
+ */
+public abstract class Person {
+	
+}
