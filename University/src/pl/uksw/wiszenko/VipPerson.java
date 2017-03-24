070d6396ea5f7b5725080fbe602376a5a3cd3021
Mikołaj Wiszenko <miwi94@o2.pl>
2017-03-24 09:41:40 +0100

    Added VipPerson

---
+++ University/src/pl/uksw/wiszenko/VipPerson.java
@@ -0,0 +1,36 @@
+package pl.uksw.wiszenko;
+
+import java.util.Calendar;
+
+public class VipPerson extends Person implements Personality {
+	private Calendar endDate;
+	private Calendar startDate;
+	
+	public VipPerson(String firstName, String lastName, boolean male, Calendar endDate) {
+		super(firstName, lastName, male);
+		this.endDate = endDate;
+	}
+
+	public int getDayToEnd(){
+		return (int) (endDate.getTimeInMillis() - startDate.getTimeInMillis()) / (1000*60*60*24);
+	}
+	
+	public Calendar getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Calendar endDate) {
+		this.endDate = endDate;
+	}
+
+	public Calendar getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Calendar startDate) {
+		this.startDate = startDate;
+	}
+	
+	
+
+}
